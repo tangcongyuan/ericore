da8bdc70040525853eb82c3520f0204102aee096
Eric Tang <tangcongyuan@gmail.com>
2017-11-06 17:20:02 -0500

    Configuration service and simple middlewares.

---
+++ Ericore/Services/Greeter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Ericore.Services
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["greeting"];
+        }
+        public string GetGreeting()
+        {
+            return "Hello from the greeter!";
+        }
+    }
+}
--- Ericore/Startup.cs
+++ Ericore/Startup.cs
@@ -1,33 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Ericore.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace Ericore
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(env.ContentRootPath)
+                            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
+            //app.UseFileServer(); // = app.UseDefaultFiles() + app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = greeter.GetGreeting();
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
